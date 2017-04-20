8527461667e48051b67e101ddcd05cb38f76cc8d
Darren Labey <dlabey@fastmail.com>
2017-04-19 20:40:51 -0700

    Adding Acitivites based on requirements

---
+++ app/src/main/java/org/wgu/termtracker/TermViewActivity.java
@@ -0,0 +1,13 @@
+package org.wgu.termtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TermViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_view);
+    }
+}
